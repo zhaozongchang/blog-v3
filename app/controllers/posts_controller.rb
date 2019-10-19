928ee65e6ca58b197960115c0d58b94a6556bf6b
赵宗昌 <xingyingsuifeng@gmail.com>
2019-10-19 16:04:29 +0800

    add user_id to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @group.user = current_user
   if @post.save
     redirect_to posts_path
   else
